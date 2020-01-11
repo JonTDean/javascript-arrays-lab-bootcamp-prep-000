3495b80346276ec7d450c21c0e9884da663195aa
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 05:53:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function appendKitten(name){
 }
 
 function prependKitten(name){
-  return kittens.concat(... kittens, "Arnold")
+  return kittens.concat( "Arnold", ... kittens)
 }
 
 function removeLastKitten(){
